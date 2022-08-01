e8a4296287bacfaa208b5258d45266b559d496fb
Yelyzaveta Demchenko <97131904+yelyzaveta05@users.noreply.github.com>
2022-08-01 18:19:45 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,22 +1,16 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-const body = document.querySelector('body')
-const changeColorBtn = document.querySelector('.change-color');
-console.log(changeColorBtn)
-changeColorBtn.addEventListener('click', function (color) {
-  const input = color.currentTarget;
-  getRandomHexColor(input);
-});
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    .toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor(){
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  const newBcgColor = bodyEl.style.backgroundColor;
+  spanEl.innerHTML = newBcgColor;
+};
